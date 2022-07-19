608517f6cc82ecd6f1a8ffa735a1f0b71af90eb7
Mahfouz-Sherif <103784823+Mahfouz-Sherif@users.noreply.github.com>
2022-07-19 14:16:13 +0200

    Add files via upload

---
+++ SalesInvoiceGenerator/src/sig/model/InvoiceLine.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sig.model;
+
+/**
+ *
+ * @author Mahfouz Sherif
+ */
+public class InvoiceLine {
+    int invoice,count;
+    String itemName;
+    double itemPrice;
+    
+}
